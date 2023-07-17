9fa024fea510f92c70847a4e04cb7fbcc7ea0e2c
IbRuslan <ribragimov2003@gmail.com>
2023-07-17 21:50:16 +0500

    create new animation

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ import {MySkills} from "./components/mySkills/MySkills";
 import {ProjectsList} from "./components/projects/ProjectsList";
 import {Contacts} from "./components/contacts/Contacts";
 
+
 function App() {
+
     return (
         <div className="App">
             <Header/>
--- src/components/aboutMe/AboutMe.tsx
+++ src/components/aboutMe/AboutMe.tsx
@@ -4,31 +4,33 @@ import s from './AboutMe.module.css'
 import * as THREE from "three";
 import NET from 'vanta/dist/vanta.net.min'
 import ava from '../../img/ava.jpg'
+import {PracticlesWrapper} from "./PracticlesWrapper";
 
 export const AboutMe = () => {
-    const myRef = useRef<HTMLDivElement>(null);
+    // const myRef = useRef<HTMLDivElement>(null);
+    //
+    // useEffect(() => {
+    //     if (myRef.current) {
+    //         const background = NET({
+    //             el: myRef.current,
+    //             THREE,
+    //             mouseControls: true,
+    //             touchControls: true,
+    //             gyroControls: false,
+    //             minHeight: 200.00,
+    //             minWidth: 200.00,
+    //             scale: 1.00,
+    //             scaleMobile: 1.00,
+    //             color: 0xf7f7f7,
+    //             backgroundColor: 0xbdb5c8
+    //         });
+    //
+    //         return () => {
+    //             background.destroy();
+    //         };
+    //     }
+    // }, []);
 
-    useEffect(() => {
-        if (myRef.current) {
-            const background = NET({
-                el: myRef.current,
-                THREE,
-                mouseControls: true,
-                touchControls: true,
-                gyroControls: false,
-                minHeight: 200.00,
-                minWidth: 200.00,
-                scale: 1.00,
-                scaleMobile: 1.00,
-                color: 0xf7f7f7,
-                backgroundColor: 0xbdb5c8
-            });
-
-            return () => {
-                background.destroy();
-            };
-        }
-    }, []);
 
     const textAnimation = {
         hidden: {
@@ -43,25 +45,26 @@ export const AboutMe = () => {
     }
 
     return (
-        <div id={'about'} ref={myRef} className={s.back}>
+        <div id={'about'} className={s.back}>
+            <PracticlesWrapper/>
             <div className={s.container}>
-                    <motion.div
-                        initial='hidden'
-                        whileInView='visible'
-                        className={s.main}>
-                        <motion.div custom={1.5} variants={textAnimation} className={s.avatar}>
-                            <img src={ava} alt="ava"/>
-                        </motion.div>
-                        <motion.div custom={1} variants={textAnimation} className={s.description}>
-                            <h1>Hi, I'm <strong>Ruslan Ibragimov</strong></h1>
-                            <div className={s.text}>
-                                <p>I’m a front-end developer who has a results-focused attitude to creating SPA, using
+                <motion.div
+                    initial='hidden'
+                    whileInView='visible'
+                    className={s.main}>
+                    <motion.div custom={1.5} variants={textAnimation} className={s.avatar}>
+                        <img src={ava} alt="ava"/>
+                    </motion.div>
+                    <motion.div custom={1} variants={textAnimation} className={s.description}>
+                        <h1>Hi, I'm <strong>Ruslan Ibragimov</strong></h1>
+                        <div className={s.text}>
+                            <p>I’m a front-end developer who has a results-focused attitude to creating SPA, using
                                 React(JS/TS), Redux. I would like to find project work and full-time employment in a
                                 company with a modern approach to development and a collaborative team.</p>
-                            </div>
-                            <a href="#!" className={s.btn}>Download CV</a>
-                        </motion.div>
+                        </div>
+                        <a href="#!" className={s.btn}>Download CV</a>
                     </motion.div>
+                </motion.div>
             </div>
         </div>
     );
